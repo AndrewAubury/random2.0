6feb250a715b6a29fa7895afee8d8ddcdf47cba0
Andrew Aubury <andrewaubury1@gmail.com>
2019-10-01 16:54:01 +0100

    Update Main.java

--- src/main/java/my/random/Main.java
+++ src/main/java/my/random/Main.java
@@ -2,5 +2,8 @@ package my.random;
 public class Main {
     public static void main(String[] args) {
         System.out.println("HEY");
+        while(int i = 1;i<11;i++){
+        System.out.println(i+"/10");
+        }
     }
 }
